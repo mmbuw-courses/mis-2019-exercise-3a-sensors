b6bcde16e17fe0c765bf3043d01c5e49657662cf
abdulmannan <abdulmannancc33@gmail.com>
2019-05-08 07:35:18 +0200

    Sensor task

--- app/src/main/java/com/example/mis/sensor/MainActivity.java
+++ app/src/main/java/com/example/mis/sensor/MainActivity.java
@@ -1,28 +1,62 @@
 package com.example.mis.sensor;
 
+import android.content.Context;
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
 import android.os.AsyncTask;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.widget.TextView;
+
 import com.example.mis.sensor.FFT;
 
 import java.util.Random;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements SensorEventListener {
 
     //example variables
     private double[] rndAccExamplevalues;
     private double[] freqCounts;
-
+    private SensorManager sensorManager;
+    Sensor accelerometer;
+    private static final String TAG="Main Activity";
+    TextView xValue,yValue,zValue;
+    //References
+    //https://developer.android.com/guide/topics/sensors/sensors_overview.html
+    //https://www.youtube.com/watch?v=pkT7DU1Yo9Q&list=LLRkVz3oUd1GQ1KNwXBC_NqA&index=3
+    //https://www.youtube.com/watch?v=Rda_5s4rObQ&list=LLRkVz3oUd1GQ1KNwXBC_NqA&index=2
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        xValue= (TextView) findViewById(R.id.xValue);
+        yValue= (TextView) findViewById(R.id.yValue);
+        zValue= (TextView) findViewById(R.id.zValue);
+
+        sensorManager =(SensorManager) getSystemService(Context.SENSOR_SERVICE);
+        accelerometer=sensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
+        sensorManager.registerListener(MainActivity.this,accelerometer,sensorManager.SENSOR_DELAY_NORMAL);
+
+
         //initiate and fill example array with random values
         rndAccExamplevalues = new double[64];
         randomFill(rndAccExamplevalues);
         new FFTAsynctask(64).execute(rndAccExamplevalues);
+    }
+    @Override
+    public void onAccuracyChanged(Sensor sensor,int i){
+
+    }
+    @Override
+    public void onSensorChanged(SensorEvent sensorEvent){
+      // Log.d (TAG,"X"+ sensorEvent.values[0]+"Y"+ sensorEvent.values[1]+"Z"+ sensorEvent.values[2]);
+        xValue.setText("X-acc" +sensorEvent.values[0]);
+        yValue.setText("Y-acc" +sensorEvent.values[1]);
+        zValue.setText("Z-acc" +sensorEvent.values[2]);
     }
 
 
